b100d2eee9a67c7caa9a1b111bc661158d2e98bb
Антон <anton-kulish15@mail.ru>
2026-09-16 12:31:17 +0700

    Кнопки: треугольник, прямоугольник, квадрат, очистка

--- laba0/MainWindow.xaml.cs
+++ laba0/MainWindow.xaml.cs
@@ -18,21 +18,14 @@ namespace laba0
     public partial class MainWindow : Window
     {
         Triangle tr;
+        Rectangle rect;
         Random rnd = new Random();
 
         public MainWindow()
         {
             InitializeComponent();
-
-            // Создание треугольника со случайными координатами
-            Point2D p1 = new Point2D(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height));
-            Point2D p2 = new Point2D(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height));
-            Point2D p3 = new Point2D(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height));
-            tr = new Triangle(p1, p2, p3);
-
-            DrawTriangle(tr);
+            CreateRandomTriangle();
         }
-
         public void DrawLine(Point2D p1, Point2D p2)
         {
             Line line = new Line();
@@ -50,9 +43,60 @@ namespace laba0
             DrawLine(tr.P2, tr.P3);
             DrawLine(tr.P3, tr.P1);
         }
+        public void DrawRectangle(Rectangle rect)
+        {
+            DrawLine(rect.TopLeft, rect.TopRight);
+            DrawLine(rect.TopRight, rect.BottomRight);
+            DrawLine(rect.BottomRight, rect.BottomLeft);
+            DrawLine(rect.BottomLeft, rect.TopLeft);
+        }
         public void ClearScene()
         {
             Scene.Children.Clear();
+        }
+        public void CreateRandomTriangle()
+        {
+            ClearScene();
+            Point2D p1 = new Point2D(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height));
+            Point2D p2 = new Point2D(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height));
+            Point2D p3 = new Point2D(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height));
+            tr = new Triangle(p1, p2, p3);
+            DrawTriangle(tr);
+        }
+        public void CreateRandomRectangle()
+        {
+            ClearScene();
+            int x = rnd.Next(10, (int)Scene.Width - 100);
+            int y = rnd.Next(10, (int)Scene.Height - 100);
+            int w = rnd.Next(50, 150);
+            int h = rnd.Next(50, 150);
+            rect = new Rectangle(new Point2D(x, y), w, h);
+            DrawRectangle(rect);
+        }
+        public void CreateRandomSquare()
+        {
+            ClearScene();
+            int x = rnd.Next(10, (int)Scene.Width - 100);
+            int y = rnd.Next(10, (int)Scene.Height - 100);
+            int size = rnd.Next(50, 150);
+            rect = new Rectangle(new Point2D(x, y), size, size);
+            DrawRectangle(rect);
+        }
+        private void BtnRandomTriangle_Click(object sender, RoutedEventArgs e)
+        {
+            CreateRandomTriangle();
+        }
+        private void BtnRandomRectangle_Click(object sender, RoutedEventArgs e)
+        {
+            CreateRandomRectangle();
+        }
+        private void BtnSquare_Click(object sender, RoutedEventArgs e)
+        {
+            CreateRandomSquare();
+        }
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            ClearScene();
         }
     }
 }
---
+++ laba0/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba0
+{
+    public class Rectangle
+    {
+        public Point2D TopLeft { get; private set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public Point2D TopRight => new Point2D(TopLeft.X + Width, TopLeft.Y);
+        public Point2D BottomLeft => new Point2D(TopLeft.X, TopLeft.Y + Height);
+        public Point2D BottomRight => new Point2D(TopLeft.X + Width, TopLeft.Y + Height);
+
+        public Rectangle(Point2D topLeft, int width, int height)
+        {
+            TopLeft = topLeft;
+            Width = width;
+            Height = height;
+        }
+
+        public void AddX(int x) => TopLeft.AddX(x);
+        
+
+        public void AddY(int y) => TopLeft.AddY(y);
+        
+    }
+}
