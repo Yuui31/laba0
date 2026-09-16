f5b89b51223998b614b804cedbf24d05c0eeaec2
Антон <anton-kulish15@mail.ru>
2026-09-16 12:06:58 +0700

    Отрисовка случайного треугольника

--- laba0/MainWindow.xaml.cs
+++ laba0/MainWindow.xaml.cs
@@ -17,12 +17,22 @@ namespace laba0
 {
     public partial class MainWindow : Window
     {
+        Triangle tr;
+        Random rnd = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Создание треугольника со случайными координатами
+            Point2D p1 = new Point2D(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height));
+            Point2D p2 = new Point2D(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height));
+            Point2D p3 = new Point2D(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height));
+            tr = new Triangle(p1, p2, p3);
+
+            DrawTriangle(tr);
         }
 
-        // Рисование линии по двум точкам
         public void DrawLine(Point2D p1, Point2D p2)
         {
             Line line = new Line();
@@ -34,16 +44,12 @@ namespace laba0
             line.Y2 = p2.Y;
             Scene.Children.Add(line);
         }
-
-        // Рисование треугольника
         public void DrawTriangle(Triangle tr)
         {
             DrawLine(tr.P1, tr.P2);
             DrawLine(tr.P2, tr.P3);
             DrawLine(tr.P3, tr.P1);
         }
-
-        // Очистка сцены
         public void ClearScene()
         {
             Scene.Children.Clear();
